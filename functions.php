12ebaa88d66043d807b9727f1d801b9e7af43bfc
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-06-30 17:10:08 -0400

    Configure new font families, style main menu

--- functions.php
+++ functions.php
@@ -11,8 +11,8 @@ define( 'CHILD_THEME_VERSION', '2.0.0' );
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
 
-	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Oswald:400,700', array(), CHILD_THEME_VERSION );
-
+//	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Oswald:400,700', array(), CHILD_THEME_VERSION );
+    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Droid+Serif|Roboto:400,400italic,700,700italic|Oswald', array(), CHILD_THEME_VERSION );
 }
 
 //* Add HTML5 markup structure
