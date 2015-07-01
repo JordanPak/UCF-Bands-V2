a0c6b003b30ccef1223a55b6b0158ca1b9741c42
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-06-30 20:55:16 -0400

    Add Masonry Scripts, work on main layout structure and styling

--- functions.php
+++ functions.php
@@ -8,24 +8,37 @@ define( 'CHILD_THEME_URL', 'http://ucfbands.com/' );
 define( 'CHILD_THEME_VERSION', '2.0.0' );
 
 //* Enqueue Google Fonts
-add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
-function genesis_sample_google_fonts() {
+add_action( 'wp_enqueue_scripts', 'ucfbands_google_fonts' );
+function ucfbands_google_fonts() {
 
-//	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Oswald:400,700', array(), CHILD_THEME_VERSION );
     wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Droid+Serif|Roboto:400,400italic,700,700italic|Oswald', array(), CHILD_THEME_VERSION );
+    
+    wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(), CHILD_THEME_VERSION );
+    
 }
+
+//* Enque JS Stuff
+add_action( 'wp_enqueue_scripts', 'ucfbands_scripts' );
+function ucfbands_scripts() {
+    
+    wp_enqueue_script( 'ucfb-masonry', get_stylesheet_directory_uri() . '/js/masonry.pkgd.min.js', array('jquery'), CHILD_THEME_VERSION );
+    wp_enqueue_script( 'ucfb-masonry-init', get_stylesheet_directory_uri() . '/js/masonry-init.js', array('jquery', 'ucfb-masonry'), CHILD_THEME_VERSION );
+}
+
 
 //* Add HTML5 markup structure
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
 
+
 //* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
 
+
 //* Add support for custom background
-add_theme_support( 'custom-background' );
+//add_theme_support( 'custom-background' );
 
 //* Add support for 3-column footer widgets
-add_theme_support( 'genesis-footer-widgets', 3 );
+//add_theme_support( 'genesis-footer-widgets', 3 );
 
 
 // Add Top Bar
@@ -33,3 +46,6 @@ add_action( 'genesis_before_header', 'add_top_bar' );
 function add_top_bar() {
     echo '<div id="top-bar"></div>';
 }
+
+
+// ISOTOPE: Add div.grid
---
+++ js/masonry-init.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function() {
+
+    jQuery('.grid').masonry({
+      // options
+      itemSelector: '.grid-item',
+      columnWidth: 200
+    });
+
+});
