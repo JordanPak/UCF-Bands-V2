f77d708e3755e6dcd76853d0bc9a3b5dd6e59396
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-09 17:49:10 -0400

    Set Page Title Background Feature w/ Styling

--- inc/page-title.php
+++ inc/page-title.php
@@ -14,8 +14,24 @@ add_action( 'genesis_before_content_sidebar_wrap', 'ucfbands_custom_page_title')
  */
 function ucfbands_custom_page_title() {
 
+    // Post ID
+    $post_ID = get_the_ID();
+    
+    // Section Classes
+    $page_title_section_classes = 'page-title';
+    
+    
     // Get Title Settings Meta
-    $remove_page_title = get_post_meta( get_the_ID(), '_ucfbands_page_title_remove_page_title', true );
+    $remove_page_title = get_post_meta( $post_ID, '_ucfbands_page_title_remove_page_title', true );
+    
+    
+    // Get Featured Image Background
+    $page_featured_image = wp_get_attachment_url( get_post_thumbnail_id( $post_ID ) );
+    
+    if ($page_featured_image) {
+        $page_featured_image = 'background-image: url(' . $page_featured_image . ') ';
+        $page_title_section_classes .= ' page-title-lg';
+    }
     
     
     // Remove Page Title
@@ -23,11 +39,9 @@ function ucfbands_custom_page_title() {
         
         ?>
         
-        <section class="page-title">
-
+        <section class="<?php echo $page_title_section_classes; ?>" style="<?php echo $page_featured_image; ?>">
 
             <?php genesis_do_post_title(); ?>
-
 
         </section>
 
