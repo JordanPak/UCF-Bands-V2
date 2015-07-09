a942fe6da9556a08499fc5dad570958ba2e9384b
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-08 20:09:56 -0400

    Clean up existing Masonry stuff

--- functions.php
+++ functions.php
@@ -27,14 +27,6 @@ function ucfbands_google_fonts() {
     
 }
 
-//* Enque JS Stuff
-add_action( 'wp_enqueue_scripts', 'ucfbands_scripts' );
-function ucfbands_scripts() {
-    
-    wp_enqueue_script( 'ucfb-masonry', get_stylesheet_directory_uri() . '/js/masonry.pkgd.min.js', array('jquery'), CHILD_THEME_VERSION );
-    wp_enqueue_script( 'ucfb-masonry-init', get_stylesheet_directory_uri() . '/js/masonry-init.js', array('jquery', 'ucfb-masonry'), CHILD_THEME_VERSION );
-}
-
 
 //* Add HTML5 markup structure
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
@@ -42,13 +34,6 @@ add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list'
 
 //* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
-
-
-//* Add support for custom background
-//add_theme_support( 'custom-background' );
-
-//* Add support for 3-column footer widgets
-//add_theme_support( 'genesis-footer-widgets', 3 );
 
 
 // Add Top Bar
