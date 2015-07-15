5c8386a80a07f0046aaa14ddae5fec2e291c5671
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-15 18:25:10 -0400

    Fixed masonry column sizer math. It works!

--- inc/js/masonry-init.js
+++ inc/js/masonry-init.js
@@ -4,44 +4,27 @@
  * Masonry JS Init
  */
 
-//jQuery(function($) {
-//    var $container = $('section.masonry-blocks');
-// 
-//    $container.imagesLoaded( function(){
-//        $container.masonry({
-//            columnWidth: '.masonry-block-sizer',
-//            gutter: '.masonry-gutter-sizer',
-//            itemSelector: '.masonry-block',
-//            percentPosition: true,
-//            isAnimated: true,
-//        });
-//    });
-//});
+// 6-Column Layout
+jQuery(document).ready( function() {
 
-
-// 6-COL GRID
-jQuery(function($) {
-//    var $container = $('section.masonry-blocks');
- 
     jQuery('.masonry-6col-grid').masonry({
         itemSelector: '.masonry-block',
         columnWidth: '.masonry-6col-grid-sizer',
         gutter: '.masonry-6col-gutter-sizer',
         percentPosition: true,
     });
+  
 });
 
 
-// 4-COL GRID
-jQuery(function($) {
-//    var $container = $('section.masonry-blocks');
- 
+// 4-Column Layout
+jQuery(document).ready( function() {
+
     jQuery('.masonry-4col-grid').masonry({
         itemSelector: '.masonry-block',
         columnWidth: '.masonry-4col-grid-sizer',
         gutter: '.masonry-4col-gutter-sizer',
         percentPosition: true,
     });
+  
 });
-
-// Sizing Stuff: http://masonry.desandro.com/options.html#element-sizing
