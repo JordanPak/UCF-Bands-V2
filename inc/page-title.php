eb8c4d440ea1807cb01748b8b5faff7f2a3f4f22
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-17 21:30:02 -0400

    Add Page title output for section grid pages. Does not show on top-level pages since top-level section pages' titles are in the header

--- inc/page-title.php
+++ inc/page-title.php
@@ -14,53 +14,61 @@ add_action( 'genesis_before_content', 'ucfbands_page_title');
  */
 function ucfbands_page_title() {
     
-    // GET ID
-    $post_ID = get_the_ID();
     
+    $page_parents = get_post_ancestors( $post );
     
-    
-    // GET OPTIONS META
-    $meta_id_prefix = '_ucfbands_page_settings_';
-    
-    $remove_title  = get_post_meta( $post_ID, $meta_id_prefix . 'remove_page_title', true );
-    $icon           = get_post_meta( $post_ID, $meta_id_prefix . 'icon', true );
-    $icon_position  = get_post_meta( $post_ID, $meta_id_prefix . 'icon_position', true );
-                
-    
-    // PROCEED IF NOT REMOVED
-    if ($remove_title == false) {
-    
+    // Only Run if Sub-page
+    if( $page_parents[0] != '' ) {
         
-        // CONFIGURE ICON
-        if ($icon) { 
-            $icon = '<i class="fa fa-' . $icon . '"></i>';
-
-            if ($icon_position === 'before') {
-                $icon_before = $icon . '&nbsp;&nbsp;';
-                $icon_after = '';
-            }
-            else {
-                $icon_before = '';
-                $icon_after = '&nbsp;&nbsp;' . $icon;
-            }
-        } // if icon set
+        
+        // GET ID
+        $post_ID = get_the_ID();
 
 
-        // OUTPUT TITLE
-        ?>
+        // GET OPTIONS META
+        $meta_id_prefix = '_ucfbands_page_settings_';
 
-        <h1 class="entry-title section-page-title" itemprop="headline">
-            <?php
-                echo $icon_before;
-                echo get_the_title();
-                echo $icon_after;
+        $remove_title  = get_post_meta( $post_ID, $meta_id_prefix . 'remove_page_title', true );
+        $icon           = get_post_meta( $post_ID, $meta_id_prefix . 'icon', true );
+        $icon_position  = get_post_meta( $post_ID, $meta_id_prefix . 'icon_position', true );
+
+
+        // PROCEED IF NOT REMOVED
+        if ($remove_title == false) {
+
+
+            // CONFIGURE ICON
+            if ($icon) { 
+                $icon = '<i class="fa fa-' . $icon . '"></i>';
+
+                if ($icon_position === 'before') {
+                    $icon_before = $icon . '&nbsp;&nbsp;';
+                    $icon_after = '';
+                }
+                else {
+                    $icon_before = '';
+                    $icon_after = '&nbsp;&nbsp;' . $icon;
+                }
+            } // if icon set
+
+
+            // OUTPUT TITLE
             ?>
-        </h1>
+
+            <h1 class="entry-title section-page-title" itemprop="headline">
+                <?php
+                    echo $icon_before;
+                    echo get_the_title();
+                    echo $icon_after;
+                ?>
+            </h1>
 
 
-        <?php
+            <?php
+
+
+        } // if title is removed
         
-        
-    } // if title is removed
+    } // if sub-page
     
 } // ucfbands_page_title()
