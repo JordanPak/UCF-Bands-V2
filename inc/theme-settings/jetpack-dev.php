f422723d0c454975113f0b427149419c6dc198c0
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-22 10:26:19 -0400

    Jetpack settings metabox registration

--- inc/theme-settings/jetpack-dev.php
+++ inc/theme-settings/jetpack-dev.php
@@ -20,3 +20,20 @@ function be_ucfbands_jetpack_settings_defaults( $defaults ) {
 }
 
 add_filter( 'genesis_theme_settings_defaults', 'be_ucfbands_jetpack_settings_defaults' );
+
+
+
+/**
+ * UCFBands Jetpack Settings Metabox Registration
+ * Register Metabox for entering the values
+ *
+ * @author Jordan Pakrosnis
+ * @link http://ucfbands.com/
+ *
+ */
+function be_ucfbands_jetpack_settings_metabox_registration( $_genesis_theme_settings_pagehook ) {
+
+    add_meta_box( 'be-jetpack-settings', 'Jetpack Settings', 'be_ucfbands_jetpack_settings_box', $_genesis_theme_settings_pagehook, 'main', 'high' );
+
+}
+add_action( 'genesis_theme_settings_metaboxes', 'be_ucfbands_jetpack_settings_metabox_registration' );
