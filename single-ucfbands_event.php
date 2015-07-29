7e88c155d97eaaf82d9e3ad83b8ef03eb4513568
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-29 16:49:43 -0400

    Keep address testing; reorganize wrapper and title. Prep for CMB2 map-picker plugin/field!

--- single-ucfbands_event.php
+++ single-ucfbands_event.php
@@ -169,8 +169,12 @@ function ucfbands_event_single_content_location() {
     
     
     //-- LOCATION --//
-    echo '<div class="one-third"><h2><i class="fa fa-map-marker"></i>&nbsp;&nbsp;Location</h2><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.91164238481!2d-81.20005989999996!3d28.602427399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7685d6a0a495f%3A0x5fd59b92b3c79bab!2sUniversity+of+Central+Florida!5e0!3m2!1sen!2sus!4v1438201178230" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
-    <br>
+    
+    // Column Wrapper & Title
+    echo '<div class="one-third"><h2><i class="fa fa-map-marker"></i>&nbsp;&nbsp;Location</h2>';
+    
+    
+    echo '
     <address>
     <b>University of Central Florida</b><br>
     4000 Central Florida Blvd<br>
