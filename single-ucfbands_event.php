03ca487eec003f5d3b2651b45025d8e786bed761
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-29 14:48:30 -0400

    Add Facebook Share Button (BETA)

--- single-ucfbands_event.php
+++ single-ucfbands_event.php
@@ -58,6 +58,20 @@ function ucfbands_event_single_date_badge() {
     
     echo $event_date;
     
+    
+    // FACEBOOK SHARE BUTTON
+    echo '<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=330141470465449";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, \'script\', \'facebook-jssdk\'));</script>';
+    
+    echo '<div class="fb-share-button" data-href="' . get_permalink() . '" data-layout="button"></div>';
+    
+    
 } // ucfbands_event_single_date_badge()
 
 
