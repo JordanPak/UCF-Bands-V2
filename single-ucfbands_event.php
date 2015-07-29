4ca8324fc9a8772fe46dc3e8049d8082307d3cf4
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-29 12:49:47 -0400

    Init CPT Single Template

---
+++ single-ucfbands_event.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ *  UCFBands CPT Single Template: Events
+ *    
+ *  @author Jordan Pakrosnis
+*/
+
+// REMOVE SIDEBAR & STANDARD LOOP //
+remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+remove_action ('genesis_loop', 'genesis_do_loop');
+
+
+
+
+
+
+//-- LOAD FRAMEWORK --//
+genesis();
