07d5cd522192677fefd4b24d4768304825faa3be
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-29 13:25:20 -0400

    Add fn to get event meta in entry hook

--- single-ucfbands_event.php
+++ single-ucfbands_event.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- *  UCFBands CPT Single Template: Events
+ *  UCFBands CPT Single Template: Event
  *    
  *  @author Jordan Pakrosnis
 */
@@ -12,10 +12,28 @@ remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
 
 
-
 // FULL-WIDTH CLASS
 require_once( CHILD_DIR . '/inc/full-width-content.php' );
 
+
+
+// GET POST & META
+add_action( 'genesis_entry_header', 'ucfbands_event_single_data' );
+
+/**
+ * UCFBands Event - Get Post & Data
+ *
+ * @author Jordan Pakrosnis
+ */
+function ucfbands_event_single_data() {
+    
+    // Get Post
+    $event = get_post();
+    
+    // Get Event meta
+    $event_meta = ucfbands_event_get_meta( $event );
+    
+} // ucfbands_event_single_data() 
 
 
 
