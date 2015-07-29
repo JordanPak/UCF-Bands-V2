e76c5d6c430a0a4749e0393d53e1d9765fa426da
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-29 16:40:01 -0400

    Add commenting & spacing fixes

--- single-ucfbands_event.php
+++ single-ucfbands_event.php
@@ -162,13 +162,13 @@ add_action( 'genesis_entry_content', 'ucfbands_event_single_content_location', 5
  */
 function ucfbands_event_single_content_location() {
     
-    // EVENT DETAILS (Content)
+    //-- POST CONTENT --//
     echo '<div class="two-thirds first">';
-        
         the_content();
-    
     echo '</div>';
     
+    
+    //-- LOCATION --//
     echo '<div class="one-third"><h2><i class="fa fa-map-marker"></i>&nbsp;&nbsp;Location</h2><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.91164238481!2d-81.20005989999996!3d28.602427399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7685d6a0a495f%3A0x5fd59b92b3c79bab!2sUniversity+of+Central+Florida!5e0!3m2!1sen!2sus!4v1438201178230" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
     <br>
     <address>
