b706c15e71822114141cddf90c07e0c972878244
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-29 16:35:15 -0400

    Add sample Google Map & Address

--- single-ucfbands_event.php
+++ single-ucfbands_event.php
@@ -151,6 +151,35 @@ function ucfbands_event_single_featured_image() {
 } // ucfbands_event_single_featured_image()
 
 
+// Remove default Post Content
+remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+add_action( 'genesis_entry_content', 'ucfbands_event_single_content', 5 );
+
+/**
+ * UCFBands Event Single - Schedule, Location, and Repitoire
+ *
+ * @author Jordan Pakrosnis
+ */
+function ucfbands_event_single_content() {
+    
+    // EVENT DETAILS (Content)
+    echo '<div class="two-thirds first"><h2>Event Details</h2>';
+        
+        the_content();
+    
+    echo '</div>';
+    
+    echo '<div class="one-third"><h2><i class="fa fa-map-marker"></i>&nbsp;&nbsp;Location</h2><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.91164238481!2d-81.20005989999996!3d28.602427399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7685d6a0a495f%3A0x5fd59b92b3c79bab!2sUniversity+of+Central+Florida!5e0!3m2!1sen!2sus!4v1438201178230" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <br>
+    <address>
+    <b>University of Central Florida</b><br>
+    4000 Central Florida Blvd<br>
+    Orlando, FL 32817
+    </address>
+    </div>';
+    
+} // ucfbands_event_single_content()
+
 
 
 add_action( 'genesis_entry_content', 'ucfbands_event_single_schedule_location_repitoire', 15 );
