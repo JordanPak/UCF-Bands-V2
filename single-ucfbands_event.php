83483b51620bb2468ddf46a18fe60cde974c78f0
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-29 13:12:53 -0400

    Remove post meta from entry footer

--- single-ucfbands_event.php
+++ single-ucfbands_event.php
@@ -6,9 +6,12 @@
  *  @author Jordan Pakrosnis
 */
 
-// REMOVE SIDEBAR & POST INFO //
+// REMOVE SIDEBAR, POST INFO, & META //
 remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
 remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+
+
 
 // FULL-WIDTH CLASS
 require_once( CHILD_DIR . '/inc/full-width-content.php' );
