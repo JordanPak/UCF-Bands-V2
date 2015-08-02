236d5fe209685d159ab36c5978acb57504e84611
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-08-01 20:19:10 -0400

    Call new functions for schedule & program output.

--- single-ucfbands_event.php
+++ single-ucfbands_event.php
@@ -291,6 +291,7 @@ function ucfbands_event_single_content() {
     if ( $show_schedule_program_wrap )
         echo '<div class="event-schedule-repitoire clearfix">';
     
+        
         // HR
         if ( $show_hr )
             echo '<hr>';
@@ -298,115 +299,31 @@ function ucfbands_event_single_content() {
     
         //-- SCHEDULE --//
         if ( $show_schedule ) {
-            echo '<div class="' . $width_schedule . ' event-schedule"><h2><i class="fa fa-list"></i>&nbsp;&nbsp;Schedule</h2>';
             
-                // Start UL
-                echo '<ul>';
-            
-                    $schedule_items = $event_meta['schedule_group'];
-
-                    //-- SCHEDULE ITEM LOOP --//
-                    foreach ( $schedule_items as $schedule_item ) {
-
-                        // Get Item Meta
-                        $time =         esc_attr( $schedule_item['time'] );
-                        $thing =        esc_attr( $schedule_item['thing'] );
-                        $sub_items =    $schedule_item['sub_item'];
-                        
-                        
-                        // Start List Item
-                        echo '<li>';
-                        
-                            // Time & Thing
-                            echo '<span>' . $time . '</span>&nbsp;&nbsp;&nbsp;&nbsp;' . $thing;
-                        
-                        
-                            // Check for sub-items
-                            if ( $sub_items != '' ) {
-                                
-                                // Nested UL
-                                echo '<ul>';
-
-                                    foreach ( $sub_items as $sub_item )
-                                        echo '<li>' . $sub_item . '</li>';
-                                
-                                echo '</ul>';
-                                
-                            } // if sub-items
-                        
-                        echo '</li>';
-
-                    } // foreach Item Loop
-            
-                echo '</ul>';
+            echo '<div class="' . $width_schedule . '">';
+                
+                // Get Schedule Items
+                $schedule_items = $event_meta['schedule_group'];
+                
+                // Get Schedule
+                echo ucfbands_event_schedule( $schedule_items );
             
             echo '</div>';
         } // show schedule
     
     
+    
         //-- PROGRAM --//
-        if ( $show_program ) {
+        if ( $show_program ) {      
             
+            echo '<div class="' . $width_program . '">';
+               
+                // Get Program & Guest Composer
+                $program =                  $event_meta['program_group'];
+                $program_guest_composer =   $event_meta['program_guest_composer'];
             
-            // WRAPPER (Style classes: "style-centered", "style-simple", "style-dotted")
-            echo '<div class="' . $width_program . ' event-program style-dotted"><h2>Program</h2>';
-            
-            
-                // Get Program with Items
-                $program = $event_meta['program_group'];
-            
-                
-                // Guest Composer
-                if ( $event_meta['program_guest_composer'] )
-                    echo '<span class="guest-composer">' . $event_meta['program_guest_composer'] . '</span>';
-            
-            
-                // Start UL
-                echo '<ul>';
-                    
-
-                    //-- PROGRAM LOOP --//
-                    foreach ( $program as $piece ) {
-
-                        // Get Item Meta
-                        $piece_title =      esc_attr( $piece['title'] );
-                        $piece_composer =   esc_attr( $piece['composer'] );
-                        $piece_notes =                $piece['piece_note'];
-                        
-                        
-                        // Piece List Item
-                        echo '<li>';
-                        
-                            // Piece Title
-                            echo '<span class="piece-title">' . $piece_title . '</span>';
-                        
-                            // Separator
-//                            echo '<span class="piece-separator">&nbsp;&nbsp;&nbsp;&nbsp;</span>';
-                        
-                            // Piece Composer
-                            echo '<span class="piece-composer">' . $piece_composer . '</span>';
-
-                        
-                            // Check for Piece Notes
-                            if ( $piece_notes != '' ) {
-
-                                // Nested UL
-                                echo '<ul>';
-
-                                    foreach ( $piece_notes as $piece_note )
-                                        echo '<li>' . $piece_note . '</li>';
-
-                                echo '</ul>';
-
-                            } // if Piece Note(s)
-                        
-                        // End Piece List Item
-                        echo '</li>';
-                        
-                    } // foreach program as piece
-            
-                // Close UL
-                echo '</ul>';
+                // Get Program
+                echo ucfbands_event_program( $program, $program_guest_composer );
             
             // Close Wrapper
             echo '</div>';
