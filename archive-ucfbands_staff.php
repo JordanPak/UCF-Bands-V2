71c306454d7e60966153c9085b038e615723d37b
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-08-07 08:57:15 -0400

    Add Non-faculty loop

--- archive-ucfbands_staff.php
+++ archive-ucfbands_staff.php
@@ -104,11 +104,12 @@ function archive_masonry_grid() {
         //-- FACULTY --//
         
         // Title
-        echo '<h1 style="display:inline-block;" class="entry-title section-page-title" itemprop="headline">' . Faculty . '</h1>';
+        echo '<h1 style="display:inline-block;" class="entry-title section-page-title" itemprop="headline">Faculty</h1>';
     
     
         // "To Staff" Button
         echo '<a style="margin-right: 3%;" class="alignright button" href="#staff">To Staff&nbsp;&nbsp;<i style="position: relative; top: -2px;" class="fa fa-sort-down"></i></a>';
+    
     
                 
         // MASONRY GRID CONTAINER //
@@ -193,40 +194,100 @@ function archive_masonry_grid() {
         // End Grid
         echo '</section>';
         
+
     
-//        //-- SORT POSTS --//
-//        
-//        $staff_faculty = array();
-//        $staff_not_faculty = array();
-//    
-//    
-//        foreach( $staff as $staff_member ) {
-//           
-//                        
-//            // Get Current Post
-//            $staff_member_post = get_post( $staff_member );
-//
-//
-//            // Get "Default" event meta (params get what we want)
-//            $staff_meta = ucfbands_staff_get_meta( $staff_member );
-//            
-//            
-//            echo $staff_member . '<br>';
-//            echo $staff_meta['position'] . '<br>';
-//            echo $staff_meta['email'] . '<br>';
-//            echo $staff_meta['is_faculty'] . '<br><br><br>';
-//            
-//            
-//            if( $staff_meta['is_faculty'] == true)
-//                $staff_faculty[] = $staff_member;
-//            else
-//                $staff_not_faculty[] = $staff_member;
-//            
-//        }
-//        
-//    
-//        echo print_r($staff_faculty);
-//        echo var_dump($staff_not_faculty);
+    
+        //-- STAFF --//
+        
+        // Title
+        echo '<br><h1 id="staff" style="display:inline-block;" class="entry-title section-page-title" itemprop="headline">Staff</h1>';
+    
+
+        // MASONRY GRID CONTAINER //
+    
+        // Column Layout
+        $masonry_column_layout = '6col';
+
+        // Use Layout Option with container output
+        echo '<section class="masonry-' . $masonry_column_layout . '-grid">';
+            echo '<div class="masonry-' . $masonry_column_layout . '-grid-sizer"></div>';
+            echo '<div class="masonry-' . $masonry_column_layout . '-gutter-sizer"></div>';
+    
+        
+        // POST LOOP //
+        foreach( $staff_not_faculty as $staff_member ) {
+            
+            
+            // Get Current Post
+            $staff_member_post = get_post( $staff_member );
+
+
+            // Get "Default" event meta (params get what we want)
+            $staff_meta = ucfbands_staff_get_meta( $staff_member );
+            
+            
+            // Format Biography
+            apply_filters( 'the_content', $staff_meta['biography'] ) ;
+
+            ?>
+            
+            <!-- MASONRY BLOCK -->
+            <div class="block masonry-block masonry-block-size--one-half">
+
+                
+                <!-- POST THUMBNAIL -->
+                <?php echo get_the_post_thumbnail( $staff_member, 'medium', array( 'class' => 'alignright staff-img' ) ); ?>
+                    
+                
+                <!-- BLOCK TITLE -->
+                <h3 class="block-header"><?php echo $staff_member_post->post_title; ?></h3>
+                
+                
+                <!-- BLOCK CONTENT -->
+                <div class="entry-content">
+                    
+                    <p>
+                    
+                        <!-- Position -->
+                        <span class="staff-position"><b><i><?php echo $staff_meta['position']; ?></i></b></span><br>
+
+
+                        <!-- Contact Info -->
+                        <?php if ( $staff_meta['email'] ) { ?>
+
+                            <br><i class="fa fa-envelope"></i> <a href="mailto:<?php echo $staff_meta['email']; ?>" target="_blank"><?php echo $staff_meta['email']; ?></a>
+                        
+                        <?php } // endif ?>
+                        
+                        
+                        <?php if ( $staff_meta['phone'] ) { ?>
+                    
+                            <br><i class="fa fa-phone"></i> <?php echo $staff_meta['phone']; ?>     
+                    
+                        <?php } //endif ?>
+                        
+                    </p>
+
+                    <!-- Biography -->
+                    <?php echo wpautop( $staff_meta['biography'] ); ?>
+                
+                </div><!-- /.entry-content -->
+
+
+            </div><!-- / .block.masonry-block -->
+            
+
+            <?php
+        
+        } // end foreach
+               
+        
+        // End Grid
+        echo '</section>';    
+    
+    
+    
+    
     
     
         // PAGINATION //
